5bbd19a725736e4cd600d769234a9b9be00543c1
Hari Ravichandran <34660389+hariravichandran@users.noreply.github.com>
2022-06-19 16:58:02 -0400

    Update nyctaxi.sql

--- nyctaxi.sql
+++ nyctaxi.sql
@@ -1,6 +1,3 @@
-// setup.sql
-// there are more columns in AnimeList.csv but i am using a simplified dataset for now
-// Data may need to be cleaned further before import
 //key,fare_amount,pickup_datetime,pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,passenger_count
 //2010-05-28 08:18:34.0000004,10.5,2010-05-28 08:18:34 UTC,-73.985881,40.7383,-73.96499,40.775237,1
 //123456789012345678901234567
